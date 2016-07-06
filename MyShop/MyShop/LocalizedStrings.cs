bd4d593c3f98e6f71f513329d0d1f3ca7a2add6f
Disni Charuka <disnicharuka@gmail.com>
2016-07-06 12:59:02 +0530

    Created ProductList and Modify Product

---
+++ MyShop/MyShop/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using MyShop.Resources;
+
+namespace MyShop
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
